221a774954feec8efbc28142e8545e9094818afb
adnanamhar123 <adnanamhar138@gmail.com>
2024-03-04 11:17:48 +0700

    second

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
+class Main {
+    public String name = "Black Rabbit";
+    public int id = 12;
+    public int age = 2;
+    public void nameRabbit() {
+        System.out.println("Nama: " +name);
+    }
+    public void idRabbit() {
+        System.out.println("Id " +id);
+    }
+    public void umurRabbit() {
+        System.out.println("Umur: " +age);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main kelinci = new Main();
+        kelinci.nameRabbit();
+        kelinci.idRabbit();;
+        kelinci.umurRabbit();
     }
 }
